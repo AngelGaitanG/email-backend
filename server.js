9a7d01ef0c851d4cd051bbdfcc4a9f2fa937f742
Angel Gabriel Gaitan <155489277+AngelGaitanG@users.noreply.github.com>
2024-07-06 22:27:12 -0300

    first commit & built server

---
+++ server.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/send-email', (req, res) => {
+    const { name, email, message } = req.body;
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.GMAIL_USER, 
+            pass: process.env.GMAIL_PASS, 
+        },
+    });
+
+    const mailOptions = {
+        from: `Contact Form <${email}>`, 
+        replyTo: email, 
+        to: 'angel.gaitan.works@gmail.com', 
+        subject: `Mensaje de ${name}`,
+        text: message,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return res.status(500).send(error.toString());
+        }
+        res.status(200).send('Correo enviado: ' + info.response);
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
